c93758360a429f476e0383a855237c861905518b
Lakshay1505 <133694401+Lakshay1505@users.noreply.github.com>
2024-01-08 19:54:07 -0500

    Update code_to_edit.py

--- code_to_edit.py
+++ code_to_edit.py
@@ -1,10 +1,10 @@
 def hi_my_name_is():
-    return "" # Write your name inside the quotation marks
+    return "Lakshay Soni" # Write your name inside the quotation marks
 
 
 def length_of_string(x):
     ## TODO: Write a code that takes as an input string (x) returns the length of the string
-    return 0 # Replace 0 by the length
+    return x # Replace 0 by the length
 
 def two_numbers_equal(x,y):
     ## TODO: Write a code that takes as an inputs two numbers (x,y) and returns True if x is equal to y and False otherwise
